81ec6f7818501761131037ccb3a7259f1b993965
Abhishek kumar <63360378+abhisingh2084@users.noreply.github.com>
2023-01-12 02:29:43 +0530

    kotlin to java

--- app/src/main/java/com/example/chatapp/Message.java
+++ app/src/main/java/com/example/chatapp/Message.java
@@ -1,4 +1,38 @@
-package com.example.chatapp;
+ package com.example.chatapp;
 
-public class Message {
+import kotlin.Metadata;
+import org.jetbrains.annotations.Nullable;
+
+ 
+public final class Message {
+   @Nullable
+   private String message;
+   @Nullable
+   private String senderId;
+
+   @Nullable
+   public final String getMessage() {
+      return this.message;
+   }
+
+   public final void setMessage(@Nullable String var1) {
+      this.message = var1;
+   }
+
+   @Nullable
+   public final String getSenderId() {
+      return this.senderId;
+   }
+
+   public final void setSenderId(@Nullable String var1) {
+      this.senderId = var1;
+   }
+
+   public Message() {
+   }
+
+   public Message(@Nullable String message, @Nullable String senderId) {
+      this.message = message;
+      this.senderId = senderId;
+   }
 }
